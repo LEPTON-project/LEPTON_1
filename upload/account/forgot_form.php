a61d7bfcf4147cc5b88370fdff2773c7f6729296
erpe <mailus@lepton-cms.org>
2013-03-12 11:32:55 +0100

    extracting forgot_form to template file

--- upload/account/forgot_form.php
+++ upload/account/forgot_form.php
@@ -8,12 +8,11 @@
  * Please see the individual license in the header of each single file or info.php of modules and templates.
  *
  * @author          Website Baker Project, LEPTON Project
- * @copyright       2004-2010, Website Baker Project
- * @copyright       2010-2011, LEPTON Project
+ * @copyright       2004-2010 Website Baker Project
+ * @copyright       2010-2013 LEPTON Project
  * @link            http://www.LEPTON-cms.org
  * @license         http://www.gnu.org/licenses/gpl.html
  * @license_terms   please see LICENSE and COPYING files in your package
- * @version         $Id: forgot_form.php 1878 2012-04-07 08:26:12Z erpe $
  *
  */
 
@@ -123,27 +122,102 @@ if(!isset($message)) {
 	$message_color = 'FF0000';
 }
 	
+/* Include  phpLib-template parser */
+require_once(WB_PATH . '/include/phplib/template.inc');
+
+// see if there exists a template file in "account-htt" folder  inside the current template
+$paths = array(
+	WB_PATH."/templates/".TEMPLATE,
+	WB_PATH."/templates/".TEMPLATE."/templates",
+	WB_PATH."/templates/".DEFAULT_TEMPLATE."/templates",
+	dirname(__FILE__) . '/htt'
+);
+
+$template_path = NULL;
+foreach($paths as $p) {
+	$temp = $p."/forgot_form.htt";
+	if (file_exists($temp)) {
+		$template_path = &$p;
+		break;
+	}
+}
+
+if ($template_path === NULL) die("Can't find a valid template for this form!");
+
+$tpl = new Template($template_path);
+
+$tpl->set_unknowns('remove');  
+
+/**
+ *	set template file name
+ *
+ */
+$tpl->set_file('forgot', 'forgot_form.htt');
+
+/**	*********
+ *	languages
+ *
+ */
+$tpl->set_block('forgot', 'languages_values_block', 'languages_values_output');
+
+$query = "SELECT `directory`,`name` from `".TABLE_PREFIX."addons` where `type`='language'";
+$result = $database->query( $query );
+if (!$result) die ($database->get_error());
+
+while( false != ($data = $result->fetchRow( MYSQL_ASSOC ) ) ) {
+
+	$sel = (LANGUAGE == $data['directory']) ? " selected='selected'" : "";
+	$tpl->set_var('LANG_SELECTED', $sel);
+	$tpl->set_var(array(
+			'LANG_CODE' 	=>	$data['directory'],
+			'LANG_NAME'		=>	$data['name']
+		)
+	);
+	$tpl->parse('languages_values_output', 'languages_values_block',true);
+}
+
+
+/**
+ *
+ *
+ */
+$hash = sha1( microtime().$_SERVER['HTTP_USER_AGENT'] );
+$_SESSION['wb_apf_hash'] = $hash;
+
+/**
+ *
+ *
+ */
+$r_value = md5( microtime(true)."sah ein knab ein roesslein stehen".$_SERVER['HTTP_USER_AGENT']);
+
+$tpl->set_var(array(
+	'TEMPLATE_DIR' 		=>	TEMPLATE_DIR,
+	'WB_URL'					=>	WB_URL,
+	'URL'			        =>	URL,
+	'FORGOT_URL'			=>	FORGOT_URL,  
+	'TEXT_FORGOT'			=>	$MENU['FORGOT'],  
+	'MESSAGE_COLOR'		=>	$message_color,
+	'MESSAGE'		      =>	$message,  
+	'TEXT_EMAIL'		  =>	$TEXT['EMAIL'],
+	'SET_DISPLAY_FORM'		  =>	'if(!isset($display_form) OR $display_form != false) {',
+	'SET_DISPLAY_FORM_END'	=>	'}',  
+	'DISPLAY_EMAIL'		=>	$email,   
+	'TEXT_SEND_DETAILS'			=>	$TEXT['SEND_DETAILS'],
+	'TEXT_LOGOUT'			=>	$MENU['LOGOUT'],
+	'TEXT_RESET'			=>	$TEXT['RESET'],
+	'HASH'						=>	$hash,
+	'TEXT_FORGOTTEN_DETAILS' => $TEXT['FORGOTTEN_DETAILS']
+	)
+);
+
+unset($_SESSION['result_message']);
+
+// for use in template <!-- BEGIN/END comment_block -->
+$tpl->set_block('forgot', 'comment_block', 'comment_replace'); 
+$tpl->set_block('comment_replace', '');
+
+// ouput the final template
+$tpl->pparse('output', 'forgot');
+
+
 ?>
-
-<h1 style="text-align: center;"><?php echo $MENU['FORGOT']; ?></h1>
-
-<form name="forgot_pass" action="<?php echo WB_URL.'/account/forgot.php'; ?>" method="post">
-	<input type="hidden" name="url" value="{URL}" />
-		<table cellpadding="5" cellspacing="0" border="0" align="center" width="500">
-		<tr>
-			<td height="40" align="center" style="color: #<?php echo $message_color; ?>;" colspan="3">
-			<?php echo $message; ?>
-			</td>
-		</tr>
-		<?php if(!isset($display_form) OR $display_form != false) { ?>
-		<tr>
-			<td height="10px" colspan="3"></td>
-		</tr>
-		<tr>
-			<td width="165" height="30" align="right"><?php echo $TEXT['EMAIL']; ?>:</td>
-			<td><input type="text" maxlength="255" name="email" value="<?php echo $email; ?>" style="width: 180px;" /></td>
-			<td><input type="submit" name="submit" value="<?php echo $TEXT['SEND_DETAILS']; ?>" style="width: 180px; font-size: 10px; color: #003366; border: 1px solid #336699; background-color: #DDDDDD; padding: 3px; text-transform: uppercase;" /></td>
-		</tr>
-		<?php } ?>
-		</table>
-</form>

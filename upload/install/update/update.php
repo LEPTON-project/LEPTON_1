820ac650b95b873d0beefdd2978daa8d7a0f663b
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-12-08 11:11:16 +0100

    fixed relative paths to config.php and to backend login

--- upload/install/update/update.php
+++ upload/install/update/update.php
@@ -20,7 +20,7 @@
 
 
 
-require_once('../config.php');
+require_once('../../config.php');
 global $admin;
 if (!is_object($admin))
 {
@@ -165,7 +165,7 @@ echo "<br /><a href='http://www.lepton-cms.org/english/contact.php' target='_bla
  *  login message
  */
 
-echo "<br /><h3><a href=\"../admins/login/index.php\">please login and check update</></h3>";
+echo "<br /><h3><a href=\"../../admins/login/index.php\">please login and check update</></h3>";
 ?>
 </div>
 <div id="update-footer">
@@ -173,7 +173,7 @@ echo "<br /><h3><a href=\"../admins/login/index.php\">please login and check upd
       <a href="http://wwww.lepton-cms.org" title="Lepton CMS">Lepton Core</a> is released under the
       <a href="http://www.gnu.org/licenses/gpl.html" title="Lepton Core is GPL">GNU General Public License</a>.
       <!-- Please note: the above reference to the GNU GPL should not be removed, as it provides a link for users to read about warranty, etc. -->
-	    <br /><a href="http://wwww.lepton-cms.org" title="Lepton CMS">Lepton CMS Package</a> is released under several different licenses.
+        <br /><a href="http://wwww.lepton-cms.org" title="Lepton CMS">Lepton CMS Package</a> is released under several different licenses.
 </div>
 </body>
 </html>

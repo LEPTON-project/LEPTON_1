bea77fd06e1835bd5c8ac922aed75f37b5d8e50a
erpe <rp@cms-lab.com>
2014-08-09 10:35:01 +0200

    undo changes

--- upload/modules/addon_file_editor/info.php
+++ upload/modules/addon_file_editor/info.php
@@ -23,10 +23,10 @@
 $module_directory   = 'addon_file_editor';
 $module_name        = 'Addon File Editor (AFE)';
 $module_function    = 'tool';
-$module_version     = '2.0.0';
+$module_version     = '1.2.2';
 $module_guid        = '8B3A91F7-E26D-4992-B4B8-FEC580F379F5';
 $module_status      = 'stable';
-$module_platform    = '2.x';
+$module_platform    = '1.x';
 $module_author      = 'Christian Sommer (doc)';
 $module_license     = '<a href="http://www.gnu.org/licenses/gpl.html">GNU General Public Licencse 3.0</a>';
 $module_license_terms = '-';
@@ -36,8 +36,6 @@ $module_description   = 'AFE allows you to edit text- and image files of install
  * ------------------------------------------------------------------------------------------------
  *	MODULE VERSION HISTORY
  * ------------------------------------------------------------------------------------------------
- *
- *	2.0.0 prepare for LEPTON 2.0.0
  *
  *	1.2.2 replace mysql_real_escape_string with addslashes
  * 

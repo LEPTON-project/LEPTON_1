73ed098fc8596f06e32102e6267b2304a2690392
erpe <rp@cms-lab.com>
2014-09-18 13:27:13 +0200

    modify update script to 1.3.2

--- upload/install/update/scripts/132_update.php
+++ upload/install/update/scripts/132_update.php
@@ -31,6 +31,9 @@ echo '<h3>Current process : updating to LEPTON 1.3.2</h3>';
  */
 $upgrade_modules = array(
     "lib_jquery",
+    "edit_area",
+	"wysiwyg_admin",
+    "wrapper",	
     "tiny_mce_jq"
 );
 
@@ -43,6 +46,60 @@ foreach ($upgrade_modules as $module)
 }
 echo "<h3>run upgrade.php of modified modules: successfull</h3>";
 
+//  write config file for LEPTON_2 
+echo '<h5>Current process : ceate new config file</h5>';
+// copy config file
+
+$file = 'config.php';
+$newfile = 'config_sik.php';
+
+if (!copy($file, $newfile)) {
+    echo "failed to copy $file...\n";
+}
+
+// prepare config file
+$config_content = "" .
+"<?php\n".
+"\n".
+"if(defined('LEPTON_PATH')) { die('By security reasons it is not permitted to load \'config.php\' twice!! ".
+"Forbidden call from \''.\$_SERVER['SCRIPT_NAME'].'\'!'); }\n\n".
+"\n\n// Upgraded to LEPTON 1.3.2\n".
+"define('DB_TYPE', 'mysql');\n".
+"define('DB_HOST', '".DB_HOST."');\n".
+"define('DB_PORT', '".DB_PORT."');\n".
+"define('DB_USERNAME', '".DB_USERNAME."');\n".
+"define('DB_PASSWORD', '".DB_PASSWORD."');\n".
+"define('DB_NAME', '".DB_NAME."');\n".
+"define('TABLE_PREFIX', '".TABLE_PREFIX."');\n".
+"\n".
+"define('LEPTON_PATH', dirname(__FILE__));\n".
+"define('LEPTON_URL', '".WB_URL."');\n".
+"define('ADMIN_PATH', LEPTON_PATH.'/admins');\n".
+"define('ADMIN_URL', LEPTON_URL.'/admins');\n".
+"\n".
+"define('LEPTON_GUID', '".LEPTON_GUID."');\n".
+"define('LEPTON_SERVICE_FOR', '');\n".
+"define('LEPTON_SERVICE_ACTIVE', 0);\n".
+"define('WB_URL', LEPTON_URL);\n".
+"define('WB_PATH', LEPTON_PATH);\n".
+"\n".
+"if (!defined('LEPTON_INSTALL')) require_once(LEPTON_PATH.'/framework/initialize.php');\n".
+"\n".
+"?>";
+
+// Check if the file exists and is writable first.
+$config_filename = 'config.php';
+if(($handle = @fopen($config_filename, 'w')) === false) {
+	set_error("Cannot open the configuration file ($config_filename)");
+} else {
+	if (fwrite($handle, $config_content, strlen($config_content) ) === false) {
+		fclose($handle);
+		set_error("Cannot write to the configuration file ($config_filename)");
+	}
+	// Close file
+	fclose($handle);
+}
+echo "<h3>config file written successfully</h3>";
 
 // at last: set db to current release-no
 $database->query('UPDATE `' . TABLE_PREFIX . 'settings` SET `value` =\'1.3.2\' WHERE `name` =\'lepton_version\'');

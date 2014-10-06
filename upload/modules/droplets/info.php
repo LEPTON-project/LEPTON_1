724f08f9e841e6ee94c56edd6871da609ca2cc7f
erpe <rp@cms-lab.com>
2014-10-06 10:56:10 +0200

    added directory export

--- upload/modules/droplets/info.php
+++ upload/modules/droplets/info.php
@@ -16,7 +16,7 @@
 $module_directory = 'droplets';
 $module_name = 'Droplets';
 $module_function = 'tool';
-$module_version = '1.5.4';
+$module_version = '1.5.5';
 $module_platform = '1.x';
 $module_author = 'Ruud, pcwacht, WebBird, Dietrich Roland Pehlke (last)';
 $module_license = 'GNU General Public License';
